5cc65ae4bf400faa1b7b67088e2574b8bf3b80ba
Alex Wilmer <djazium@gmail.com>
2018-10-21 17:37:42 -0400

    ✨ hit stack api

--- functions/ask.js
+++ functions/ask.js
@@ -1,20 +1,35 @@
 exports.handler = (event, context, callback) => {
+  let https = require('https')
   let qs = require('querystring')
-  let body = qs.parse(event.body)
 
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      response_type: 'in_channel',
-      text: `Question: ${body.text}`,
-      attachments: [
-        {
-          text: '...',
-        },
-      ],
-    }),
+  let { text } = qs.parse(event.body)
+
+  let params = qs.stringify({
+    q: text,
   })
+
+  https.get(
+    `https://api.stackexchange.com/search/advanced?site=stackoverflow.com?${params}`,
+    resp => {
+      let data = ''
+      resp.on('data', chunk => (data += chunk))
+      resp.on('end', () => {
+        callback(null, {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            response_type: 'in_channel',
+            text: `Hopefully one of these answers your question!`,
+            attachments: [
+              {
+                text: data.slice(0, 100),
+              },
+            ],
+          }),
+        })
+      })
+    },
+  )
 }
