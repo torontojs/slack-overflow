e6bcb1148e4e898a72f2655cfc4ec5d2deb9dc7f
Alex Wilmer <djazium@gmail.com>
2018-10-21 17:44:14 -0400

    🐛fix params to stack

--- functions/ask.js
+++ functions/ask.js
@@ -2,34 +2,32 @@ exports.handler = (event, context, callback) => {
   let https = require('https')
   let qs = require('querystring')
 
-  let { text } = qs.parse(event.body)
+  let { text = '' } = qs.parse(event.body)
 
   let params = qs.stringify({
+    site: 'stackoverflow.com',
     q: text,
   })
 
-  https.get(
-    `https://api.stackexchange.com/search/advanced?site=stackoverflow.com?${params}`,
-    resp => {
-      let data = ''
-      resp.on('data', chunk => (data += chunk))
-      resp.on('end', () => {
-        callback(null, {
-          statusCode: 200,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            response_type: 'in_channel',
-            text: `Hopefully one of these answers your question!`,
-            attachments: [
-              {
-                text: JSON.parse(data).has_more,
-              },
-            ],
-          }),
-        })
+  https.get(`https://api.stackexchange.com/search/advanced?${params}`, resp => {
+    let data = ''
+    resp.on('data', chunk => (data += chunk))
+    resp.on('end', () => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          response_type: 'in_channel',
+          text: `Hopefully one of these answers your question!`,
+          attachments: [
+            {
+              text: JSON.parse(data).has_more,
+            },
+          ],
+        }),
       })
-    },
-  )
+    })
+  })
 }
