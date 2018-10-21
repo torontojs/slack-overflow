b28e492ddcd8cd000faadaf2597414c03d0e1019
Alex Wilmer <djazium@gmail.com>
2018-10-21 16:44:53 -0400

    💬 echo cmd

--- functions/ask.js
+++ functions/ask.js
@@ -1,6 +1,11 @@
 exports.handler = (event, context, callback) => {
   callback(null, {
-    statusCode: 200,
-    body: 'Hello, World',
+    response_type: 'in_channel',
+    text: 'hey hey hey hey',
+    attachments: [
+      {
+        text: event.body,
+      },
+    ],
   })
 }
